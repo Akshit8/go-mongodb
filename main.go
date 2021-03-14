1619f9f1d332b23b3dfb83f4240d7c1a6e2819fb
Akshit Sadana <akshitsadana@gmail.com>
2021-03-14 23:17:10 +0530

    main function

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Akshit8/go-mongodb/repository/mongo"
+
+	"github.com/Akshit8/go-mongodb/config"
+)
+
+var noteRepo mongo.NoteRepository
+
+func main() {
+	var appConfig config.AppConfig
+	err := config.LoadConfig("cmd/config", &appConfig)
+	if err != nil {
+		log.Fatalln("error loading config: ", err)
+	}
+
+	timeout := time.Duration(10) * time.Second
+	client, err := mongo.NewMongoClient(appConfig.MongoURI, timeout)
+	if err != nil {
+		log.Fatalln("error connecting to mongodb: ", err)
+	}
+
+	noteRepo = mongo.NewNoteRepository(client, appConfig.DBName, appConfig.NotesTable, timeout)
+}
+
+func createNote() {}
+
+func getNote() {}
