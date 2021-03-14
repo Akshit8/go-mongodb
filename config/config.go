86e4b20d16ad70af3f2c99cd960ecc1c2c3c8dbc
Akshit Sadana <akshitsadana@gmail.com>
2021-03-14 16:37:27 +0530

    pkg config added

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+// AppConfig stores all configuration of the application.
+type AppConfig struct {
+	MongoURI   string `mapstructure:"MONGO_URI"`
+	NotesTable string `mapstructure:"NOTES_TABLE"`
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string, config *AppConfig) error {
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	viper.AutomaticEnv()
+
+	err = viper.Unmarshal(config)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Akshit8/go-mongodb
 
 go 1.15
 
-require go.mongodb.org/mongo-driver v1.4.6
+require (
+	github.com/spf13/viper v1.7.1
+	go.mongodb.org/mongo-driver v1.4.6
+)
